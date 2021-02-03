94ceb00c24ccd3f58655aa3d132854c64c6ea0ce
henry <48340699+hYe0kjUn@users.noreply.github.com>
2021-02-04 01:21:30 +0900

    fix: volatility test
    fix: volatility test

--- test/VolatilityBreakthrough.py
+++ test/VolatilityBreakthrough.py
@@ -1,45 +1,45 @@
 from pystock_hts import Daishin
 
+k_list = [0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
-stock_list = ["A005930", "A252670", "A114800", "A251340", "A122630", "A233740"] #삼성전자, KODEX 200선물인버스 2X, KODEX 인버스, KODEX 코스닥 150선물인버스, KODEX 레버리지, KODEX 코스닥150 레버리지
-
-k = 0.5
-average = None
-before_last_price = 0
-win = 0
-lose = 0
-revenue = 0
-
-for stock_code in stock_list:
-  date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list = Daishin.CpSysDib().getStockChartPriceToDate(60, stock_code)
+for k in k_list:
+  all_revenue_of_k = 0
+  print("k: ", k)
   
-  average = None
-  before_last_price = 0
-  win = 0
-  lose = 0
-  revenue = 0
+  stock_list = ["A005930", "A252670", "A114800", "A251340", "A122630", "A233740"] #삼성전자, KODEX 200선물인버스 2X, KODEX 인버스, KODEX 코스닥 150선물인버스, KODEX 레버리지, KODEX 코스닥150 레버리지
 
-  for date, high_price, low_price, last_price in zip(date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list):
-    if not average:
-      average = int((high_price - low_price) * k)
-      before_last_price = last_price
-      continue
+  for stock_code in stock_list:
+    date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list = Daishin.CpSysDib().getStockChartPriceToDate(60, stock_code)
     
-    goal_price = before_last_price + average
-    
-    if goal_price > high_price:
-      before_last_price = last_price
-      continue
+    average = None
+    before_last_price = 0
+    win = 0
+    lose = 0
+    revenue = 0
+
+    for date, high_price, low_price, last_price in zip(date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list):
+      if not average:
+        average = int((high_price - low_price) * k)
+        before_last_price = last_price
+        continue
       
-    if goal_price < last_price:
-      win += 1
-    else:
-      lose += 1
+      goal_price = before_last_price + average
       
-    revenue += last_price - goal_price
-    before_last_price = last_price
+      if goal_price >= high_price:
+        before_last_price = last_price
+        continue
+        
+      if goal_price < last_price:
+        win += 1
+      else:
+        lose += 1
+        
+      revenue += last_price - goal_price
+      before_last_price = last_price
+      
+    print(stock_code)
+    print("revenue: ", revenue)
+    print("win: ", win)
+    print("lose: ", lose)
     
-  print(stock_code)
-  print("revenue: ", revenue)
-  print("win: ", win)
-  print("lose: ", lose)
+  print("all_revenue_of_k: ", all_revenue_of_k)
