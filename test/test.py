a5b27437c3f44f0d34e61548d485a2ad815efca0
henry <48340699+hYe0kjUn@users.noreply.github.com>
2021-02-04 01:03:38 +0900

    test: test api
    test: test api

--- test/test.py
+++ test/test.py
@@ -5,18 +5,20 @@ daishin_util = Daishin.CpUtil()
 daishin_trade = Daishin.CpTrade()
 
 is_connected = daishin_util.getConnected()
+print(f"is_connected: {is_connected}")
+
 stock_name = daishin_util.getStockCodeToName("A005930")
+print(f"stock_name: {stock_name}")
 
 stock_price = daishin_sys.getStockChartPriceToDate(2, "A005930")
+print(f"stock_price: {stock_price}")
+
 stock_per = daishin_sys.getStockPer("A005930")
+print(f"stock_per: {stock_per}")
 
 is_buyed = daishin_trade.buyStock("A005930")
-is_selled = daishin_trade.sellStock("A005930")
-
-print(f"is_connected: {is_connected}")
-print(f"stock_name: {stock_name}")
-print(f"stock_price: {stock_price}")
-print(f"stock_per: {stock_per}")
 print(f"is_buyed: {is_buyed}")
+
+is_selled = daishin_trade.sellStock("A005930")
 print(f"is_selled: {is_selled}")
       
