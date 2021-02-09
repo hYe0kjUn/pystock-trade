ec5d7e7e7303681a7e982a227eff873f850a2a6f
hYe0kjUn <soc06103@gmail.com>
2021-02-09 10:12:54 +0900

    fix: test

---
+++ Util/UtilService.py
@@ -0,0 +1,2 @@
+def getKValue(high_price_list, low_price_list, last_price_list):
+    
--- test/VolatilityBreakthrough.py
+++ test/VolatilityBreakthrough.py
@@ -1,17 +1,49 @@
+from ast import Num
 from pystock_hts import Daishin
 import time
 
+def getKValue(high_price_list, low_price_list, last_price_list, start_price_list):
+  high_price_all = 0
+  low_price_all = 0
+  last_price_all = 0
+  start_price_all = 0
+  i = 0
+  for high_price, low_price, last_price, start_price in zip(high_price_list, low_price_list, last_price_list, start_price_list):
+    high_price_all += Num(high_price)
+    low_price_all += Num(low_price)
+    last_price_all += Num(last_price)
+    start_price_all += Num(start_price)
+    i += 1
+    if i > 20:
+      i -= 1
+    high_price_all -= Num(high_price)
+    low_price_all -= Num(low_price)
+    last_price_all -= Num(last_price)
+    start_price_all -= Num(start_price)
+
+  return 1-abs((start_price_all - last_price_all)/(high_price_all - low_price_all))
+      
+
 k = 0.5
-ratelimit = 0
 all_revenue_of_k = 0
 
-stock_list = ["A005930", "A252670", "A114800", "A251340", "A122630", "A233740"] #삼성전자, KODEX 200선물인버스 2X, KODEX 인버스, KODEX 코스닥 150선물인버스, KODEX 레버리지, KODEX 코스닥150 레버리지
+stock_list = ["A005930", "A114800", "A069500", "A229200", "A251340"] #삼성전자, KODEX 인버스, KODEX 200, KODEX 코스닥 150, KODEX 코스닥150선물인버스
 
 for stock_code in stock_list:
+  print("wait")
+  time.sleep(3)
+  print("wait done")
   
-  date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list = Daishin.CpSysDib().getStockChartPriceToDate(30, stock_code)
-  ratelimit += 4
+  date_list, stock_start_price_list, stock_high_price_list, stock_low_price_list, stock_last_price_list = Daishin.CpSysDib().getStockChartPriceToDate(21, stock_code)
   
+  del date_list[0]
+  del stock_start_price_list[0]
+  del stock_high_price_list[0]
+  del stock_low_price_list[0]
+  del stock_last_price_list[0]
+
+  k = getKValue(date_list, stock_low_price_list, stock_last_price_list, stock_start_price_list)
+
   average = None
   before_last_price = 0
   win = 0
@@ -19,6 +51,7 @@ for stock_code in stock_list:
   revenue = 0
 
   for date, high_price, low_price, last_price in zip(date_list, stock_high_price_list, stock_low_price_list, stock_last_price_list):
+    print(date, high_price, low_price, last_price)
     if not average:
       average = int((high_price - low_price) * k)
       before_last_price = last_price
@@ -38,14 +71,13 @@ for stock_code in stock_list:
     revenue = revenue + last_price - goal_price
     before_last_price = last_price
     
+  print(Daishin.CpUtil().getStockCodeToName(stock_code))
   print(stock_code)
   print("revenue: ", revenue)
   print("win: ", win)
   print("lose: ", lose)
   all_revenue_of_k += revenue
-  print("wait")
-  time.sleep(15)
-  
+
 print(f"{k}", all_revenue_of_k)
 print()
 print()
