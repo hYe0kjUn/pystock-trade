486ce190d11e7f192f3d3286d7bd614257760e48
henry <48340699+hYe0kjUn@users.noreply.github.com>
2021-02-09 10:39:11 +0900

    fix: trade

--- TradeService/VolatilityBreakthrough.py
+++ TradeService/VolatilityBreakthrough.py
@@ -36,4 +36,5 @@ def VolatilityBreakthroughSellBot():
 
 date_list, start_price_list, high_price_list, low_price_list, last_price_list = Daishin.CpSysDib().getStockChartPriceToDate(21, "A229200")
 
+print(date_list, start_price_list, high_price_list, low_price_list, last_price_list)
 print(getKValue(high_price_list, low_price_list, last_price_list, start_price_list))
